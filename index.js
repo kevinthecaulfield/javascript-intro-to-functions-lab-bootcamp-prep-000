0fc946fb998f408a9b7bf0547b3fefcecf9cc35c
Kevin Smith <kevincaulfield630@gmail.com>
2017-10-23 23:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+  if (string.toUpperCase() === string) {
+    return "YES INDEED"
   }
   else if (string === "I love you, Grandma") {
     return "I love you, too."
   }
   else {
-    return ("YES INDEED!")
+    return ("I can't hear you!")
   }
 }
