11b7df944f77815ea5261eefe8d35aad6744515f
Kevin Smith <kevincaulfield630@gmail.com>
2017-10-23 23:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if (string === "I love you, Grandma") {
     return "I love you, too."
   }
-  else {
+  else if (string.toUpperCase() === string){
     return ("YES INDEED!")
   }
 }
